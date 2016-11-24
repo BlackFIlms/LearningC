419445318de0fcc769d70d38b0705fea75a2760e
BlackFilms <blackfilms13@gmail.com>
2016-11-24 22:33:46 +0500

    Add StepOne and write basic operations

---
+++ StepOne - BasicConcepts/StepOne - BasicConcepts.cpp
@@ -0,0 +1,15 @@
+/*
+This file it explains the basic things in C++ language.
+Such as: basic operations, functions, standart IO functions.
+*/
+
+#include <stdio.h> //Include - basic operation for connect library. <stdio.h> - standart library for IO functions in C++.
+#include <conio.h> //Include console IO functions.
+#include <iostream> //Connect library to organize IO functions
+
+using namespace std; //Provide acces to std namespace
+
+int main(void) //Primary function with empty arguments -> (void)
+{
+
+}
