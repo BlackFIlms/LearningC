b85dc4c130d51930e21f79686947b7654c24e529
BlackFilms <blackfilms13@gmail.com>
2016-11-29 02:00:33 +0500

    addOnePrintFunction

--- LearningC/TaskOne-mathSquareOfTheTwoNumbers.cpp
+++ LearningC/TaskOne-mathSquareOfTheTwoNumbers.cpp
@@ -15,6 +15,7 @@ int math(int x)
 int main(void)
 {
 	int x;
+	printf("Enter random number, for math sqaure of the numbers \n"); //print: request nember
 	scanf_s("%i", &x); //input x
 	printf("x = %i \n", math(x)); //output result function math
 	_getch();
