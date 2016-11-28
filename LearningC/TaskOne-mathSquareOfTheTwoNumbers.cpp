5fa6027dd6265e80825a880b899887fedfefbcc8
BlackFilms <blackfilms13@gmail.com>
2016-11-28 18:36:14 +0500

    Comments in TaskOne

--- LearningC/TaskOne-mathSquareOfTheTwoNumbers.cpp
+++ LearningC/TaskOne-mathSquareOfTheTwoNumbers.cpp
@@ -1,16 +1,21 @@
+/*
+Task:
+Write math squad of the numbers.
+*/
+
 #include <stdio.h>
 #include <conio.h>
 using namespace std;
 
 int math(int x)
 {
-	return x*x;
+	return x*x; //return and math squad of the numbers.
 }
 
 int main(void)
 {
 	int x;
-	scanf_s("%i", &x);
-	printf("x = %i \n", math(x));
+	scanf_s("%i", &x); //input x
+	printf("x = %i \n", math(x)); //output result function math
 	_getch();
 }
