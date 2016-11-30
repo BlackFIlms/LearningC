94840085cf91979d26db5284c0d029c0f54fd591
BlackFilms <blackfilms13@gmail.com>
2016-11-30 14:56:58 +0500

    Add many comments and write first condition.

--- LearningC - StepTwo - Loops/StepTwo - Loops.cpp
+++ LearningC - StepTwo - Loops/StepTwo - Loops.cpp
@@ -15,44 +15,68 @@ int main(void)
 		cout << "Hi there!" << endl;	/*body of the loop*/	//
 	}															//
 
-	cout << ">>>>>>>> End of for loop! <<<<<<<<" << endl;	// Marking for console
+	cout << ">>>>>>>> End of for loop! <<<<<<<<" << endl;	// Marking for console.
 
 	int y = 1;
 
 	while (y == 1)									// While loop, this loop working, as the condition in brackets is true!
 	{												//
-		cout << "y == 1" << endl;					// if y == 1 loop started and print in console this text
-		cout << "enter new value for Y:" << endl;	// Request from user new value for Y
-		cin >> y;									// Request from user new value for Y
+		cout << "y == 1" << endl;					// if y == 1 loop started and print in console this text.
+		cout << "enter new value for Y:" << endl;	// Request from user new value for Y.
+		cin >> y;									// Request from user new value for Y.
 		if (y != 1)									//
-		{											// check and print to console: y == 1 or no
+		{											// check and print to console: y == 1 or no.
 			cout << "Y != 1" << endl;				//
 		}											//
 	}
 
-	cout << "y == ";								// if while loop end working, print new Y value
-	cout << y << endl;								// if while loop end working, print new Y value
+	cout << "y == ";								// if while loop end working, print new Y value.
+	cout << y << endl;								// if while loop end working, print new Y value.
 
-	cout << ">>>>>>>> End of while loop! <<<<<<<<" << endl;	// Marking for console
+	cout << ">>>>>>>> End of while loop! <<<<<<<<" << endl;	// Marking for console.
 
 	do {															// do While loop, this same While loop. But the condition is checked after executing loop body.
 		if (y != 1)													//
 		{															//
-			cout << "Y != 1" << endl;								// check and print Y == 1 or no
+			cout << "Y != 1" << endl;								// check and print Y == 1 or no.
 		}															//
 		if (y == 1)													//
 		{															//
-			cout << "Y == 1" << endl;								// check and print Y == 1 or no
+			cout << "Y == 1" << endl;								// check and print Y == 1 or no.
 		}															//
-		cout << "enter new value for Y:" << endl;					// Request from user new value for Y
-		cin >> y;													// Request from user new value for Y
-		if (y != 1)													// check and print Y == 1 or no
+		cout << "enter new value for Y:" << endl;					// Request from user new value for Y.
+		cin >> y;													// Request from user new value for Y.
+		if (y != 1)													// check and print Y == 1 or no.
 		{															//
 			cout << "Y == ";										//
 			cout << y << endl;										//
 			cout << "This is finish for: do-while!" << endl;		//
 		}															//
-	} while (y == 1);												// check Y == 1 or no
+	} while (y == 1);												// check Y == 1 or no.
+
+	cout << ">>>>>>>> End of do-while loop! <<<<<<<<" << endl;	// Marking for console.
+
+	cout << "						" << endl;	//
+	cout << "						" << endl;	// indent for console.
+	cout << "						" << endl;	//
+
+	int variable;
+
+	cout << "Please insert value for variable:" << endl;
+	cin >> variable;
+
+	if (variable < 0)												// If - standard test conditions for the expression.
+	{																//
+		cout << "You insert number < 0 in variable!" << endl;		//
+	}																//
+	else if (variable == 0)											// Else if - used when more than two conditions.
+	{																//
+		cout << "You insert 0 in variable!" << endl;				//
+	}																//
+	else															// Else - used as the default value.
+	{																//
+		cout << "You insert number > 0 in variable!" << endl;		//
+	}																//
 
 	_getch();
 }
